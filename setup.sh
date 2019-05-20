be9f459b9011e3dbbf0f05d5702424a2c38cfe63
Roy Chan <nerdroychan@users.noreply.github.com>
2019-05-20 18:14:46 +0800

    Update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,20 @@
-curr=`pwd`
-echo "Current directory:" $curr
+DIR=`pwd`
+echo "Current directory:" $DIR
 
-# Vim
-ln -s $curr/vim ~/.vim
+# vim
+rm -r $HOME/.vim
+ln -sf $DIR/vim $HOME/.vim
+
+# i3
+rm -r $HOME/.config/i3
+mkdir -p $HOME/.config/i3
+ln -sf $DIR/i3/* $HOME/.config/i3
+
+# kitty
+rm -r $HOME/.config/kitty
+mkdir -p $HOME/.config/kitty
+ln -sf $DIR/kitty/*.conf $HOME/.config/kitty
+
+# tmux
+rm $HOME/.tmux.conf
+ln -sf $DIR/tmux/tmux.conf $HOME/.tmux.conf
