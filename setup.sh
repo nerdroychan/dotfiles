184416df3216a59e2bceaae43a6679f3f6e2b484
Chen Chen <mail@roychan.org>
2019-05-21 13:17:11 +0800

    Update

--- setup.sh
+++ setup.sh
@@ -2,25 +2,24 @@ DIR=`pwd`
 echo "Current directory:" $DIR
 
 # vim
-rm -r $HOME/.vim
 ln -sf $DIR/vim $HOME/.vim
 
 # i3
-rm -r $HOME/.config/i3
 mkdir -p $HOME/.config/i3
 ln -sf $DIR/i3/* $HOME/.config/i3
 
 # kitty
-rm -r $HOME/.config/kitty
 mkdir -p $HOME/.config/kitty
 ln -sf $DIR/kitty/*.conf $HOME/.config/kitty
 
 # tmux
-rm $HOME/.tmux.conf
 ln -sf $DIR/tmux/tmux.conf $HOME/.tmux.conf
 
 # xinitrc
-ln -sf $DIR/xinitrc $HOME/.xinitrc.conf
+ln -sf $DIR/xinitrc $HOME/.xinitrc
 
 # imwheel
 ln -sf $DIR/imwheelrc $HOME/.imwheelrc
+
+# supervisord
+ln -sf $DIR/xsupervisord $HOME/.xsupervisord
