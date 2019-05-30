2a0f24e0d1ac2630beceaa2a1e1cb131d4f18195
Roy Chan <nerdroychan@users.noreply.github.com>
2019-05-30 19:40:40 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,27 +4,27 @@ DIR=$(cd `dirname $0` && pwd)
 echo "Current directory:" $DIR
 
 # vim
-rm $HOME/.vim
-ln -sf $DIR/vim $HOME/.vim
+rm "$HOME"/.vim
+ln -sf "$DIR"/vim "$HOME"/.vim
 
-mkdir -p $HOME/.config/i3
-rm $HOME/.config/i3/*
-ln -sf $DIR/i3/* $HOME/.config/i3
+mkdir -p "$HOME"/.config/i3
+rm "$HOME"/.config/i3/*
+ln -sf "$DIR"/i3/* "$HOME"/.config/i3
 
 # kitty
-mkdir -p $HOME/.config/kitty
-rm $HOME/.config/kitty/*
-ln -sf $DIR/kitty/*.conf $HOME/.config/kitty
+mkdir -p "$HOME"/.config/kitty
+rm "$HOME"/.config/kitty/*
+ln -sf "$DIR"/kitty/*.conf "$HOME"/.config/kitty
 
 # tmux
-ln -sf $DIR/tmux/tmux.conf $HOME/.tmux.conf
+ln -sf "$DIR"/tmux/tmux.conf "$HOME"/.tmux.conf
 
 # xinitrc
-ln -sf $DIR/xinitrc $HOME/.xinitrc
+ln -sf "$DIR"/xinitrc "$HOME"/.xinitrc
 
 # xkbmap
-rm $HOME/.xkbmap
-ln -sf $DIR/xkbmap $HOME/.xkbmap
+rm "$HOME"/.xkbmap
+ln -sf "$DIR"/xkbmap "$HOME"/.xkbmap
 
 # zsh
-ln -sf $DIR/zsh/zshrc $HOME/.zshrc
+ln -sf "$DIR"/zsh/zshrc "$HOME"/.zshrc
