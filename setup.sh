007ff7148e68fd3889288853968c32c12f7aea4d
Roy Chan <nerdroychan@users.noreply.github.com>
2019-07-11 09:30:40 +0800

    Add alacritty

--- setup.sh
+++ setup.sh
@@ -13,12 +13,17 @@ ln -sf "$DIR"/vim "$HOME"/.vim
 
 mkdir -p "$HOME"/.config/i3
 rm "$HOME"/.config/i3/*
-ln -sf "$DIR"/i3/* "$HOME"/.config/i3
+ln -sf "$DIR"/i3/* "$HOME"/.config/i3/
 
 # kitty
 mkdir -p "$HOME"/.config/kitty
 rm "$HOME"/.config/kitty/*
-ln -sf "$DIR"/kitty/*.conf "$HOME"/.config/kitty
+ln -sf "$DIR"/kitty/*.conf "$HOME"/.config/kitty/
+
+# alacritty
+mkdir -p "$HOME"/.config/alacritty
+rm "$HOME"/.config/alacritty/*
+ln -sf "$DIR"/alacritty/*.yml "$HOME"/.config/alacritty/
 
 # tmux
 ln -sf "$DIR"/tmux/tmux.conf "$HOME"/.tmux.conf
