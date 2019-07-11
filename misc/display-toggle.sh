a65c0b0b65fcd24f05ff422008fdabbd45a4d914
Chen Chen <mail@roychan.org>
2019-07-11 21:34:02 +0800

    Update misc

--- misc/display-toggle.sh
+++ misc/display-toggle.sh
@@ -6,16 +6,18 @@ INTERNAL_OUTPUT="eDP1"
 mode=$1"m"
 
 if [ $mode = "em" ]; then
-        xrandr --output $INTERNAL_OUTPUT --off \
-               --output $EXTERNAL_OUTPUT --auto --mode 3840x2160 \
-               --fb 3840x2160 --dpi 192
+    xrandr --output $INTERNAL_OUTPUT --off \
+           --output $EXTERNAL_OUTPUT --auto --mode 3840x2160 \
+           --fb 3840x2160 --dpi 192
 elif [ $mode = "im" ]; then
-        xrandr --output $EXTERNAL_OUTPUT --off \
-               --output $INTERNAL_OUTPUT --auto --mode 1920x1080 \
-               --fb 1920x1080 --dpi 144
+    xrandr --output $EXTERNAL_OUTPUT --off \
+           --output $INTERNAL_OUTPUT --auto --mode 1920x1080 \
+           --fb 1920x1080 --dpi 144
 elif [ $mode = "bm" ]; then
-        xrandr --fb 7680x4320 --dpi 192 \
-               --output $EXTERNAL_OUTPUT --auto --mode 3840x2160 \
-               --output $INTERNAL_OUTPUT --auto --scale 2x2 --pos 3840x0 \
-                                         --panning 3840x2160+3840+0
+    xrandr --fb 7680x4320 --dpi 192 \
+           --output $EXTERNAL_OUTPUT --auto --mode 3840x2160 \
+           --output $INTERNAL_OUTPUT --auto --scale 2x2 --pos 3840x0 \
+                                     --panning 3840x2160+3840+0
+else
+    xrandr --auto
 fi
