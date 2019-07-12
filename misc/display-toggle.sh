82e81fa8e5457f463094516f1cb27fa10aa3504d
Chen Chen <mail@roychan.org>
2019-07-12 20:28:12 +0800

    Update 4k

--- misc/display-toggle.sh
+++ misc/display-toggle.sh
@@ -6,21 +6,15 @@ INTERNAL_OUTPUT="eDP1"
 mode=$1"m"
 
 if [ $mode = "em" ]; then
-    xrandr --output $EXTERNAL_OUTPUT --auto --output $INTERNAL_OUTPUT --auto && \
-    xrandr --output $INTERNAL_OUTPUT --off \
-           --output $EXTERNAL_OUTPUT --auto --mode 3840x2160 \
-           --fb 3840x2160
+    xrandr --output $EXTERNAL_OUTPUT --auto --output $INTERNAL_OUTPUT --auto
+    xrandr --output $EXTERNAL_OUTPUT --auto --output $INTERNAL_OUTPUT --off
 elif [ $mode = "im" ]; then
-    xrandr --output $EXTERNAL_OUTPUT --auto --output $INTERNAL_OUTPUT --auto && \
-    xrandr --output $EXTERNAL_OUTPUT --off \
-           --output $INTERNAL_OUTPUT --auto --mode 1920x1080 \
-           --fb 1920x1080
+    xrandr --output $EXTERNAL_OUTPUT --auto --output $INTERNAL_OUTPUT --auto
+    xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --off
 elif [ $mode = "bm" ]; then
-    xrandr --output $EXTERNAL_OUTPUT --auto --output $INTERNAL_OUTPUT --auto && \
-    xrandr --fb 7680x4320 \
-           --output $EXTERNAL_OUTPUT --auto --mode 3840x2160 \
-           --output $INTERNAL_OUTPUT --auto --scale 2x2 --pos 3840x0 \
-                                     --panning 3840x2160+3840+0
+    xrandr --output $EXTERNAL_OUTPUT --auto --output $INTERNAL_OUTPUT --auto
+    xrandr --output $EXTERNAL_OUTPUT --auto \
+           --output $INTERNAL_OUTPUT --auto --right-of $EXTERNAL_OUTPUT
 else
     xrandr --output $EXTERNAL_OUTPUT --auto --output $INTERNAL_OUTPUT --auto
 fi
