5cf372814eb4a85c1f2f07b1517086a8f37b7d28
Roy Chan <nerdroychan@users.noreply.github.com>
2019-07-12 22:15:00 +0800

    Update i3

--- i3/startup.sh
+++ i3/startup.sh
@@ -2,9 +2,10 @@
 
 feh --bg-fill $HOME/.wallpaper.jpg
 
-killall -w fcitx copyq redshift-gtk pasystray
+killall -w fcitx copyq redshift-gtk pasystray flameshot
 
 fcitx &
 redshift-gtk -l 31:121 -t 6500:5000 &
 pasystray &
 copyq &
+flameshot &
