5a9c26a7803d5ddaa864543c62f591479fa52d97
Chen Chen <mail@roychan.org>
2019-09-20 15:52:05 +0800

    Update

--- misc/pulseaudio-toggle.sh
+++ misc/pulseaudio-toggle.sh
@@ -4,7 +4,9 @@ mode=$1"m"
 
 if [ $mode = "am" ]; then
     pactl set-card-profile 0 output:analog-stereo
-elif [ $mode = "dm" ]; then
+elif [ $mode = "d1m" ]; then
+    pactl set-card-profile 0 output:hdmi-stereo
+elif [ $mode = "d2m" ]; then
     pactl set-card-profile 0 output:hdmi-stereo-extra1
 else
     pacmd list-cards | grep 'active profile'
