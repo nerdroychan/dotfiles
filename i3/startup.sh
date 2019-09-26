a4a6fc3eb1d8fe5e36aff61ae4820c24dd36aafc
Chen Chen <nerdroychan@users.noreply.github.com>
2019-09-26 16:12:21 +0800

    Update dropbox

--- i3/startup.sh
+++ i3/startup.sh
@@ -2,10 +2,11 @@
 
 feh --bg-fill $HOME/.wallpaper.jpg
 
-killall -w fcitx copyq redshift-gtk pasystray flameshot
+killall -w fcitx copyq redshift-gtk pasystray flameshot dropbox
 
 fcitx &
 redshift-gtk -l 31:121 -t 6500:5000 &
 pasystray -m 100 &
 copyq &
 flameshot &
+dropbox &
