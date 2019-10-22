4f381c228d6205b0c03ae5fb1700103a451c2087
Chen Chen <nerdroychan@users.noreply.github.com>
2019-10-22 19:44:45 +0800

    Update

--- misc/display-toggle.sh
+++ misc/display-toggle.sh
@@ -7,10 +7,10 @@ INTERNAL_OUTPUT="eDP1"
 
 mode=$1"m"
 
-xrandr --output $INTERNAL_OUTPUT --auto \
-       --output $EXTERNAL_OUTPUT --auto \
-       --output $EXTERNAL_OUTPUT2 --auto \
-       --output $EXTERNAL_OUTPUT3 --auto
+# xrandr --output $INTERNAL_OUTPUT --auto \
+#        --output $EXTERNAL_OUTPUT --auto \
+#        --output $EXTERNAL_OUTPUT2 --auto \
+#        --output $EXTERNAL_OUTPUT3 --auto
 
 if [ $mode = "em" ]; then
     xrandr --output $INTERNAL_OUTPUT --off \
