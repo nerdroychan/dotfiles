525a4b74c87013a516f7c50338ad41c8fa694aa9
Chen Chen <mail@roychan.org>
2019-10-24 15:23:48 +0800

    Update

--- misc/backup.sh
+++ misc/backup.sh
@@ -4,8 +4,7 @@ DESTINATION=/run/media/chen/backup
 HOME=/home/chen
 FILE=$DESTINATION/$(date "+%F-%T").tar.gz
 
-tar -g $HOME/.backup-inc-meta \
-    --exclude=$HOME/.cache \
+tar --exclude=$HOME/.cache \
     --exclude=$HOME/AUR \
     --exclude-caches \
     --ignore-failed-read \
