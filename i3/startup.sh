b92eb5ccef2d0e61c3f67e25f314fb2cf25874e1
Chen Chen <nerdroychan@users.noreply.github.com>
2020-01-04 13:50:44 -0600

    Update nm-applet

--- i3/startup.sh
+++ i3/startup.sh
@@ -2,7 +2,7 @@
 
 feh --bg-fill $HOME/.wallpaper.jpg
 
-killall -w fcitx copyq redshift-gtk pasystray flameshot dropbox
+killall -w fcitx copyq redshift-gtk pasystray flameshot dropbox nm-applet
 
 fcitx &
 redshift-gtk -t 6500:5000 &
@@ -10,3 +10,4 @@ pasystray -m 100 &
 copyq &
 flameshot &
 dropbox &
+nm-applet &
