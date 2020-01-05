de343c87d3f46f84924723f2bb1c6e5c7d7b2a68
Chen Chen <nerdroychan@users.noreply.github.com>
2020-01-05 14:41:48 -0600

    Update

--- i3/startup.sh
+++ i3/startup.sh
@@ -4,11 +4,11 @@ feh --bg-fill $HOME/.wallpaper.jpg
 
 killall -w fcitx copyq redshift-gtk pasystray flameshot dropbox nm-applet
 
-firejail fcitx &
-firejail redshift-gtk -t 6500:5000 &
-firejail pasystray -m 100 &
-firejail copyq &
-firejail dropbox &
-firejail nm-applet &
+fcitx &
+redshift-gtk -t 6500:5000 &
+pasystray -m 100 &
+copyq &
+nm-applet &
 
 unshare -n -U flameshot &
+firejail dropbox &
