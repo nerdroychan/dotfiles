16f1f6b08a494d93d6feb0f48f2ab8c25891c851
Chen Chen <mail@roychan.org>
2020-04-08 13:07:54 -0500

    back to ryzen...

--- i3/startup.sh
+++ i3/startup.sh
@@ -2,10 +2,11 @@
 
 feh --bg-fill $HOME/.wallpaper.jpg &
 
-killall -w copyq redshift-gtk flameshot fcitx5
+killall -w copyq redshift-gtk flameshot fcitx5 #picom
 
 redshift-gtk -t 6500:5000 &
 copyq &
 fcitx5 &
 
 flameshot &
+# picom -CG &
