95313439ae1731ef37fb5ab2a8340fac7dee9981
Chen Chen <nerdroychan@users.noreply.github.com>
2020-04-29 17:59:10 -0500

    update for desktop

--- i3/startup.sh
+++ i3/startup.sh
@@ -2,11 +2,11 @@
 
 feh --bg-fill $HOME/.wallpaper.jpg &
 
-killall -w copyq redshift-gtk flameshot fcitx #picom
+killall -w copyq redshift-gtk flameshot fcitx picom
 
 redshift-gtk -t 6500:5000 &
 copyq &
 fcitx &
 
 flameshot &
-# picom -CG &
+picom -CG &
