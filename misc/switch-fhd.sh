3f633b70a3771672c934a6162b19a0d4a5e8cd29
Chen Chen <mail@roychan.org>
2020-06-09 10:39:30 -0500

    update

--- misc/switch-fhd.sh
+++ misc/switch-fhd.sh
@@ -2,5 +2,5 @@
 
 printf "Xft.dpi: 144" | xrdb -merge
 printf "*dpi: 144" | xrdb -merge
-xrdb -merge $HOME/.config/polybar/default.xresources
+xrdb -merge $HOME/.config/polybar/fhd.xresources
 i3-msg restart &> /dev/null
