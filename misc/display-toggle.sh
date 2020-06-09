4f96ddffad62a682e8565d0247e8b9db560da791
Chen Chen <nerdroychan@users.noreply.github.com>
2020-06-09 10:39:30 -0500

    update

--- misc/display-toggle.sh
+++ misc/display-toggle.sh
@@ -1,9 +1,10 @@
 #/bin/sh
 
-EXTERNAL_OUTPUT1="HDMI-A-0"
-EXTERNAL_OUTPUT2="DisplayPort-0"
-EXTERNAL_OUTPUT3="DisplayPort-1"
-INTERNAL_OUTPUT="eDP"
+EXTERNAL_OUTPUT0="HDMI-1"
+EXTERNAL_OUTPUT1="HDMI-2"
+EXTERNAL_OUTPUT2="DP-1"
+EXTERNAL_OUTPUT3="DP-2"
+INTERNAL_OUTPUT="eDP-1"
 
 mode=$1"m"
 
@@ -14,11 +15,13 @@ mode=$1"m"
 
 if [ $mode = "em" ]; then
     xrandr --output $INTERNAL_OUTPUT --off \
+           --output $EXTERNAL_OUTPUT0 --auto \
            --output $EXTERNAL_OUTPUT1 --auto \
            --output $EXTERNAL_OUTPUT2 --auto \
            --output $EXTERNAL_OUTPUT3 --auto
 elif [ $mode = "im" ]; then
     xrandr --output $INTERNAL_OUTPUT --auto \
+           --output $EXTERNAL_OUTPUT0 --off \
            --output $EXTERNAL_OUTPUT1 --off \
            --output $EXTERNAL_OUTPUT2 --off \
            --output $EXTERNAL_OUTPUT3 --off
