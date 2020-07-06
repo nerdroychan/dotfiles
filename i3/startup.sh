e517f7ce3311ceb3b39244841ea12bc1c2316d8a
Chen Chen <mail@roychan.org>
2020-07-05 22:02:02 -0500

    update

--- i3/startup.sh
+++ i3/startup.sh
@@ -4,7 +4,7 @@ feh --bg-fill $HOME/.wallpaper.jpg &
 
 killall -w redshift-gtk flameshot fcitx picom
 
-redshift-gtk -t 6500:5000 &
+redshift-gtk -t 6500:5000 -l 41.88:-87.62 &
 fcitx &
 
 flameshot &
