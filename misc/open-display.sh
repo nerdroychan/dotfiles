ab95fc9c6117121594ace3425f2dfad95c86ce61
Chen Chen <nerdroychan@users.noreply.github.com>
2020-11-19 12:42:15 -0600

    update xrandr udev

--- misc/open-display.sh
+++ misc/open-display.sh
@@ -1,3 +1,15 @@
 #/bin/sh
 
+export USER="chen"
+export HOME="/home/chen"
+export DISPLAY=":0"
+export XAUTHORITY="/home/chen/.Xauthority"
+
+sleep 1
+xrandr >/dev/null 2>/dev/null
 xrandr --auto
+sleep 2
+xrandr >/dev/null 2>/dev/null
+xrandr --auto
+
+exit 0
