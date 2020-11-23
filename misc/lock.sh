2b4892982d59a7b708ca902192be9dbc0f5cdb7e
Chen Chen <mail@roychan.org>
2020-11-22 21:25:12 -0600

    update lock

--- misc/lock.sh
+++ misc/lock.sh
@@ -2,4 +2,7 @@
 
 env XSECURELOCK_PASSWORD_PROMPT=time_hex \
     XSECURELOCK_BLANK_DPMS_STATE=on \
+    XSECURELOCK_SAVER=saver_mpv \
+    XSECURELOCK_IMAGE_DURATION_SECONDS=inf \
+    XSECURELOCK_LIST_VIDEOS_COMMAND="echo ~/.wallpaper.jpg" \
     xsecurelock
