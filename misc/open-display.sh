26ad5bfcfe7e63412d61cc1465769196cf035d82
Chen Chen <nerdroychan@users.noreply.github.com>
2020-12-02 14:35:48 -0600

    revert

--- misc/open-display.sh
+++ misc/open-display.sh
@@ -4,17 +4,7 @@ export USER="chen"
 export HOME="/home/chen"
 export DISPLAY=":0"
 export XAUTHORITY="/home/chen/.Xauthority"
-#export LOCK="/tmp/.DISPLAYLOCK"
 
 sleep 1
-
-#mkdir $LOCK >/dev/null 2>/dev/null || exit 1
-
-#trap "rm -rf ${LOCK}; exit" INT TERM EXIT
-
 xrandr >/dev/null 2>/dev/null
 xrandr --auto
-
-i3-msg restart >/dev/null 2>/dev/null
-
-#rm -rf $LOCK
