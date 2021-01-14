f84a7ce03db3c8c7ab8701dcc72717f146b22782
Chen Chen <nerdroychan@users.noreply.github.com>
2021-01-13 19:16:30 -0600

    lock

--- setup.sh
+++ setup.sh
@@ -77,7 +77,6 @@ ln -sf $DIR/misc/switch-uhd.sh $HOME/.switch-uhd.sh
 
 # lock
 ln -sf $DIR/misc/lock.sh $HOME/.lock.sh
-ln -sf $DIR/misc/lock.jpg $HOME/.lock.jpg
 
 # mpv
 mkdir -p $HOME/.config/mpv
