d171713e4a1ede7e32aa4e3383117e4c7e702dcc
Chen Chen <mail@roychan.org>
2021-02-05 15:03:24 -0600

    update dual monitor

--- polybar/startup.sh
+++ polybar/startup.sh
@@ -2,8 +2,15 @@
 
 killall -q polybar
 
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+
+echo $PRIMARY
+echo $OTHERS
+
 if type "xrandr"; then
-    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload default &> /tmp/polybar-$m.log &
+    env MONITOR=$PRIMARY TRAY=right polybar --reload default &> /tmp/polybar-$MONITOR.log &
+    for m in $OTHERS; do
+        env MONITOR=$m polybar --reload default &> /tmp/polybar-$MONITOR.log &
     done
 fi
