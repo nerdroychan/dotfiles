6bc06ebb762c5ba72426062dcc04d1a18bcbad05
Chen Chen <mail@roychan.org>
2021-09-26 23:34:59 -0500

    vfio

--- vfio/unbind.sh
+++ vfio/unbind.sh
@@ -2,15 +2,17 @@ GPU=11:00
 GPU_ID="1002 73ff"
 GPU_AUDIO_ID="1002 ab28"
 
-# remove pcie dev
-echo "1" > /sys/bus/pci/devices/"0000:${GPU}.0"/remove
-echo "1" > /sys/bus/pci/devices/"0000:${GPU}.1"/remove
-sleep 1
+# Unbind GPU from vfio-pci
+echo -n "0000:${GPU}.0" > /sys/bus/pci/drivers/vfio-pci/unbind || echo "Failed to unbind gpu from vfio-pci"
+echo -n "0000:${GPU}.1" > /sys/bus/pci/drivers/vfio-pci/unbind || echo "Failed to unbind gpu-audio from vfio-pci"
 
-# remove id
+# Remove GPU id
 echo -n "$GPU_ID" > /sys/bus/pci/drivers/vfio-pci/remove_id
 echo -n "$GPU_AUDIO_ID" > /sys/bus/pci/drivers/vfio-pci/remove_id
 
+# remove pcie dev
+echo "1" > /sys/bus/pci/devices/"0000:${GPU}.0"/remove
+echo "1" > /sys/bus/pci/devices/"0000:${GPU}.1"/remove
 sleep 1
 
 # add back to default driver
