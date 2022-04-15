d592b051686aefb4f5558c9f2108dd0d816cbdb9
Chen Chen <mail@roychan.org>
2022-04-15 08:43:27 -0500

    inotifywait and picom

--- i3/startup.sh
+++ i3/startup.sh
@@ -7,3 +7,31 @@ fcitx5 &
 
 flameshot &
 picom -CGb &
+
+# The access to xdg user directories should be rare..
+function monitor_xdg_dirs {
+    LOGFILE=/tmp/inotifywait.log
+
+    killall -w inotifywait
+
+    if ! [ -x "$(command -v inotifywatch)" ]; then
+        i3-nagbar -m 'Please install inotify-tools' -t warning &> /dev/null
+    fi
+
+    touch $LOGFILE
+    inotifywait -m -o $LOGFILE \
+                --timefmt '%c' \
+                --format '%T [%e]: %w %f' \
+                -e open,modify,delete \
+                -r \
+                $HOME/Archives \
+                $HOME/Desktop \
+                $HOME/Documents \
+                $HOME/Downloads \
+                $HOME/Music \
+                $HOME/Pictures \
+                $HOME/Public \
+                $HOME/Templates \
+                $HOME/Videos &
+}
+monitor_xdg_dirs
