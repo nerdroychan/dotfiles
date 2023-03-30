06c1fd80b789fcb0eaed63312e0953c6ba2db589
Chen Chen <mail@roychan.org>
2023-03-30 14:17:33 -0500

    scripts: update duplicity script with target nickname

--- scripts/duplicity-home.sh
+++ scripts/duplicity-home.sh
@@ -2,29 +2,31 @@
 
 ME=`basename "$0"`
 
-if [ $# -ne 3 ]; then
-    echo "Usage: $ME <enckey> <signkey> <destination>"
+if [ $# -ne 4 ]; then
+    echo "Usage: $ME <enckey> <signkey> <destination> <identifier>"
     exit 0;
 fi
 
 ENCKEY=$1
 SIGNKEY=$2
 DESTINATION=$3
+IDENTIFIER=$4
 
-echo "enckey: $ENCKEY, signkey: $SIGNKEY, destination: $DESTINATION"
+echo "enckey: $ENCKEY, signkey: $SIGNKEY, destination: $DESTINATION, identifier: $IDENTIFIER"
 read -p "continue? [y/N] " r
 
-if [ "$r" == "y" ] && [ ! -z $DESTINATION ]; then
+if [ "$r" == "y" ] && [ ! -z "$DESTINATION" ]; then
     duplicity --verbosity info \
               --num-retries 3 \
               --progress \
               --volsize 500 \
               --use-agent \
-              --encrypt-key $ENCKEY \
-              --sign-key $SIGNKEY \
+              --name "$IDENTIFIER" \
+              --encrypt-key "$ENCKEY" \
+              --sign-key "$SIGNKEY" \
               --exclude "$HOME/.*" \
-              $HOME/ \
-              $DESTINATION/
+              "$HOME/" \
+              "$DESTINATION/"
 else
     exit 0
 fi
