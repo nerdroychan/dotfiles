07536a01397ed1c17c33aaf16b85cfc54062b077
Chen Chen <mail@roychan.org>
2024-02-06 13:03:32 -0600

    polybar: remove network

--- polybar/startup.sh
+++ polybar/startup.sh
@@ -3,7 +3,7 @@
 PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
 OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
 
-export DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
+# export DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
 
 echo "Primary: "$PRIMARY
 echo "Others: "$OTHERS
