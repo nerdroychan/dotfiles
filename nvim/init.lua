5115e4bdbf105278ae77c9802227772a3518d3b6
Chen Chen <mail@roychan.org>
2024-04-12 22:52:01 -0500

    nvim: revert matchup offscreen style

--- nvim/init.lua
+++ nvim/init.lua
@@ -132,9 +132,6 @@ require("lazy").setup({
     {
         "andymass/vim-matchup",
         lazy = false,
-        config = function()
-            vim.g.matchup_matchparen_offscreen = { method = "popup" }
-        end,
     },
     -- auto close parentheses
     {
