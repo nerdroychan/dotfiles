b3f149124e8be3340d3ca9333852c53b9f7490c8
Chen Chen <mail@roychan.org>
2024-09-14 20:43:20 -0400

    i3: set processes as child

--- i3/startup.sh
+++ i3/startup.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 # with tray icons
 unshare -c -n flameshot &
 unshare -c -n fcitx5 &
 
 # headless, assuming they won't crash..
-unshare -c -n redshift -t 6500:5000 -l 41.88:-87.62 &
-unshare -c -n picom -CGb &
+unshare -c -n redshift -t 6500:5000 -l 42.74:-84.48 &
+unshare -c -n picom -CG &
 
 # xss-lock to lock on suspend
-xss-lock -- $HOME/.lock.sh
+xss-lock -- $HOME/.lock.sh &
 
-exit 0
+wait
