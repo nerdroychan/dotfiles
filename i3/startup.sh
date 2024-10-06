91a5677a5113f106020bab568ae697a8ee43df44
Chen Chen <mail@roychan.org>
2024-10-06 14:50:19 -0400

    i3: update picom arguments

--- i3/startup.sh
+++ i3/startup.sh
@@ -6,7 +6,7 @@ unshare -c -n fcitx5 &
 
 # headless, assuming they won't crash..
 unshare -c -n redshift -t 6500:5000 -l 42.74:-84.48 &
-unshare -c -n picom -CG &
+unshare -c -n picom &
 
 # xss-lock to lock on suspend
 xss-lock -- $HOME/.lock.sh &
