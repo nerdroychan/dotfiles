cc5bf16f914a3d735a351caa11a0232deb60ba26
Chen Chen <mail@roychan.org>
2025-05-16 09:30:28 -0400

    gtk: keep cantarell as default font in gtk4

--- setup.sh
+++ setup.sh
@@ -99,7 +99,9 @@ function __rofi {
 function __gtk {
     ln -sf $DIR/gtk/gtk-2.0/gtkrc-2.0 $HOME/.gtkrc-2.0
     mkdir -p $HOME/.config/gtk-3.0 &> /dev/null
+    mkdir -p $HOME/.config/gtk-4.0 &> /dev/null
     ln -sf $DIR/gtk/gtk-3.0/settings.ini $HOME/.config/gtk-3.0/
+    ln -sf $DIR/gtk/gtk-4.0/settings.ini $HOME/.config/gtk-4.0/
 }
 
 function __gnupg {
